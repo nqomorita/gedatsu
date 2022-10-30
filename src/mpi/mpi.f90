c741549dd9b93f0fa1fdbb51a47b220d4bc7c9db
Naoki MORITA <nqo.morita@gmail.com>
2022-10-30 16:40:24 +0900

    Add mpi

--- src/mpi/mpi.f90
+++ src/mpi/mpi.f90
@@ -5,23 +5,9 @@ module mod_gedatsu_mpi
   implicit none
   private
 
-  public :: gedatsu_allreduce_I1
-  public :: gedatsu_allreduce_I
-  public :: gedatsu_allreduce_R1
-  public :: gedatsu_allreduce_R
-  public :: gedatsu_Isend_I
-  public :: gedatsu_Irecv_I
-  public :: gedatsu_Isend_R
-  public :: gedatsu_Irecv_R
-  public :: gedatsu_gatherv_I
-  public :: gedatsu_gatherv_R
-  public :: gedatsu_scatterv_I
-  public :: gedatsu_scatterv_R
-  public :: gedatsu_allgather_I1
-
-  integer(gint), parameter :: tagSum = 1
-  integer(gint), parameter :: tagMax = 2
-  integer(gint), parameter :: tagMin = 3
+  integer(gint), parameter :: gedatsu_mpi_sum = 1
+  integer(gint), parameter :: gedatsu_mpi_max = 2
+  integer(gint), parameter :: gedatsu_mpi_min = 3
 
 contains
 
@@ -34,11 +20,11 @@ contains
     in = val
     out = 0
     n = 1
-    if(tag == tagSum)then
+    if(tag == gedatsu_mpi_sum)then
       call MPI_allreduce(in, out, n, MPI_INTEGER, MPI_SUM, comm, ierr)
-    elseif(tag == tagMax)then
+    elseif(tag == gedatsu_mpi_max)then
       call MPI_allreduce(in, out, n, MPI_INTEGER, MPI_MAX, comm, ierr)
-    elseif(tag == tagMin)then
+    elseif(tag == gedatsu_mpi_min)then
       call MPI_allreduce(in, out, n, MPI_INTEGER, MPI_MIn, comm, ierr)
     endif
     val = out(1)
@@ -52,11 +38,11 @@ contains
 
 #ifndef WITH_NOMPI
     temp = 0
-    if(tag == tagSum)then
+    if(tag == gedatsu_mpi_sum)then
       call MPI_allreduce(val, temp, n, MPI_INTEGER, MPI_SUM, comm, ierr)
-    elseif(tag == tagMax)then
+    elseif(tag == gedatsu_mpi_max)then
       call MPI_allreduce(val, temp, n, MPI_INTEGER, MPI_MAX, comm, ierr)
-    elseif(tag == tagMin)then
+    elseif(tag == gedatsu_mpi_min)then
       call MPI_allreduce(val, temp, n, MPI_INTEGER, MPI_MIn, comm, ierr)
     endif
     val = temp
@@ -73,11 +59,11 @@ contains
     in = val
     out = 0.0d0
     n = 1
-    if(tag == tagSum)then
+    if(tag == gedatsu_mpi_sum)then
       call MPI_allreduce(in, out, n, MPI_REAL8, MPI_SUM, comm, ierr)
-    elseif(tag == tagMax)then
+    elseif(tag == gedatsu_mpi_max)then
       call MPI_allreduce(in, out, n, MPI_REAL8, MPI_MAX, comm, ierr)
-    elseif(tag == tagMin)then
+    elseif(tag == gedatsu_mpi_min)then
       call MPI_allreduce(in, out, n, MPI_REAL8, MPI_MIn, comm, ierr)
     endif
     val = out(1)
@@ -92,11 +78,11 @@ contains
 
 #ifndef WITH_NOMPI
     temp = 0.0d0
-    if(tag == tagSum)then
+    if(tag == gedatsu_mpi_sum)then
       call MPI_allreduce(val, temp, n, MPI_REAL8, MPI_SUM, comm, ierr)
-    elseif(tag == tagMax)then
+    elseif(tag == gedatsu_mpi_max)then
       call MPI_allreduce(val, temp, n, MPI_REAL8, MPI_MAX, comm, ierr)
-    elseif(tag == tagMin)then
+    elseif(tag == gedatsu_mpi_min)then
       call MPI_allreduce(val, temp, n, MPI_REAL8, MPI_MIn, comm, ierr)
     endif
     val = temp
@@ -150,10 +136,10 @@ contains
     integer(gint) :: comm
     integer(gint) :: sbuf(:)
     integer(gint) :: rbuf(:)
+    integer(gint) :: ierr
 
 #ifndef WITH_NOMPI
-    integer(gint) :: ierr
-    call MPI_gatherv( sbuf, sc, MPI_REAL8, rbuf, rcs, disp, MPI_REAL8, root, comm, ierr )
+    call MPI_gatherv(sbuf, sc, MPI_REAL8, rbuf, rcs, disp, MPI_REAL8, root, comm, ierr)
 #else
     rbuf(1:sc) = sbuf(1:sc)
 #endif
@@ -168,10 +154,10 @@ contains
     integer(gint) :: comm
     integer(gint) :: sbuf(:)
     integer(gint) :: rbuf(:)
+    integer(gint) :: ierr
 
 #ifndef WITH_NOMPI
-    integer(gint) :: ierr
-    call MPI_scatterv( sbuf, scs, disp, MPI_REAL8, rbuf, rc, MPI_REAL8, root, comm, ierr )
+    call MPI_scatterv(sbuf, scs, disp, MPI_REAL8, rbuf, rc, MPI_REAL8, root, comm, ierr)
 #else
     rbuf(1:rc) = sbuf(1:rc)
 #endif
@@ -186,10 +172,10 @@ contains
     integer(gint) :: comm
     real(gdouble) :: sbuf(:)
     real(gdouble) :: rbuf(:)
+    integer(gint) :: ierr
 
 #ifndef WITH_NOMPI
-    integer(gint) :: ierr
-    call MPI_gatherv( sbuf, sc, MPI_REAL8, rbuf, rcs, disp, MPI_REAL8, root, comm, ierr )
+    call MPI_gatherv(sbuf, sc, MPI_REAL8, rbuf, rcs, disp, MPI_REAL8, root, comm, ierr)
 #else
     rbuf(1:sc) = sbuf(1:sc)
 #endif
@@ -204,10 +190,10 @@ contains
     integer(gint) :: comm
     real(gdouble) :: rbuf(:)
     real(gdouble) :: sbuf(:)
+    integer(gint) :: ierr
 
 #ifndef WITH_NOMPI
-    integer(gint) :: ierr
-    call MPI_scatterv( sbuf, scs, disp, MPI_REAL8, rbuf, rc, MPI_REAL8, root, comm, ierr )
+    call MPI_scatterv(sbuf, scs, disp, MPI_REAL8, rbuf, rc, MPI_REAL8, root, comm, ierr)
 #else
     rbuf(1:rc) = sbuf(1:rc)
 #endif
@@ -218,13 +204,123 @@ contains
     integer(gint) :: sval
     integer(gint) :: rbuf, out(1)
     integer(gint) :: comm
+    integer(gint) :: ierr
 
 #ifndef WITH_NOMPI
-    integer(gint) :: ierr
-    call MPI_allgather(sval, 1, MPI_INTEGER, out, 1, MPI_INTEGER, comm, ierr )
+    call MPI_allgather(sval, 1, MPI_INTEGER, out, 1, MPI_INTEGER, comm, ierr)
     rbuf = out(1)
 #else
     rbuf(1) = sval
 #endif
   end subroutine gedatsu_allgather_I1
+
+  subroutine gedatsu_SendRecv_R(send_n_neib, send_neib_pe, recv_n_neib, recv_neib_pe, &
+    & send_index, send_item, recv_index, recv_item, &
+    & ws, wr, val, ndof, comm)
+    implicit none
+    integer(gint) :: send_n_neib, recv_n_neib
+    integer(gint) :: iS, in, j, k, ierr
+    integer(gint) :: i, ndof, comm
+    integer(gint), pointer :: send_neib_pe(:)
+    integer(gint), pointer :: send_index(:)
+    integer(gint), pointer :: send_item (:)
+    integer(gint), pointer :: recv_neib_pe(:)
+    integer(gint), pointer :: recv_index(:)
+    integer(gint), pointer :: recv_item (:)
+    integer(gint) :: sta1(gedatsu_mpi_status_size, send_n_neib)
+    integer(gint) :: sta2(gedatsu_mpi_status_size, recv_n_neib)
+    integer(gint) :: req1(send_n_neib)
+    integer(gint) :: req2(recv_n_neib)
+    real(gdouble) :: val(:), ws(:), wr(:)
+
+#ifndef WITH_NOMPI
+    do i = 1, send_n_neib
+      iS = send_index(i - 1)
+      in = send_index(i  ) - iS
+      if(in == 0) cycle
+      do j = iS + 1, iS + in
+        do k = 1, ndof
+          ws(ndof*(j - 1) + k) = val(ndof*(send_item(j) - 1) + k)
+        enddo
+      enddo
+      call gedatsu_Isend_R(ndof*in, ws(ndof*iS + 1:ndof*iS + ndof*in), send_neib_pe(i), comm, req1(i))
+    enddo
+
+    do i = 1, recv_n_neib
+      iS = recv_index(i - 1)
+      in = recv_index(i  ) - iS
+      if(in == 0) cycle
+      call gedatsu_Irecv_R(ndof*in, ws(ndof*iS + 1:ndof*iS + ndof*in), recv_neib_pe(i), comm, req2(i))
+    enddo
+
+    call MPI_waitall(recv_n_neib, req2, sta2, ierr)
+
+    do i = 1, recv_n_neib
+      iS = recv_index(i - 1)
+      in = recv_index(i  ) - iS
+      do j = iS + 1, iS + in
+        do k = 1, ndof
+          val(ndof*(recv_item(j) - 1) + k) = wr(ndof*(j - 1) + k)
+        enddo
+      enddo
+    enddo
+
+    call MPI_waitall(send_n_neib, req1, sta1, ierr)
+#endif
+  end subroutine gedatsu_SendRecv_R
+
+  subroutine gedatsu_SendRecv_I(send_n_neib, send_neib_pe, recv_n_neib, recv_neib_pe, &
+    & send_index, send_item, recv_index, recv_item, &
+    & ws, wr, val, ndof, comm)
+    implicit none
+    integer(gint) :: send_n_neib, recv_n_neib
+    integer(gint) :: iS, in, j, k, ierr
+    integer(gint) :: i, ndof, comm
+    integer(gint), pointer :: send_neib_pe(:)
+    integer(gint), pointer :: send_index(:)
+    integer(gint), pointer :: send_item (:)
+    integer(gint), pointer :: recv_neib_pe(:)
+    integer(gint), pointer :: recv_index(:)
+    integer(gint), pointer :: recv_item (:)
+    integer(gint) :: sta1(gedatsu_mpi_status_size, send_n_neib)
+    integer(gint) :: sta2(gedatsu_mpi_status_size, recv_n_neib)
+    integer(gint) :: req1(send_n_neib)
+    integer(gint) :: req2(recv_n_neib)
+    integer(gint) :: val(:), ws(:), wr(:)
+
+#ifndef WITH_NOMPI
+    do i = 1, send_n_neib
+      iS = send_index(i-1)
+      in = send_index(i  ) - iS
+      if(in == 0) cycle
+      do j = iS + 1, iS + in
+        do k = 1, ndof
+          ws(ndof*(j - 1) + k) = val(ndof*(send_item(j) - 1) + k)
+        enddo
+      enddo
+      call gedatsu_Isend_I(ndof*in, ws(ndof*iS + 1:ndof*iS + ndof*in), send_neib_pe(i), comm, req1(i))
+    enddo
+
+    do i = 1, recv_n_neib
+      iS = recv_index(i-1)
+      in = recv_index(i  ) - iS
+      if(in == 0) cycle
+      call gedatsu_Irecv_I(ndof*in, ws(ndof*iS + 1:ndof*iS + ndof*in), recv_neib_pe(i), comm, req2(i))
+    enddo
+
+    call MPI_waitall(recv_n_neib, req2, sta2, ierr)
+
+    do i = 1, recv_n_neib
+      iS = recv_index(i - 1)
+      in = recv_index(i  ) - iS
+      do j = iS + 1, iS + in
+        do k = 1, ndof
+          val(ndof*(recv_item(j) - 1) + k) = wr(ndof*(j - 1) + k)
+        enddo
+      enddo
+    enddo
+
+    call MPI_waitall(send_n_neib, req1, sta1, ierr)
+#endif
+  end subroutine gedatsu_SendRecv_I
 end module mod_gedatsu_mpi
