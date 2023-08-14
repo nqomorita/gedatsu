c298b4e2697fd46d18fe0f83d7d11c26d6dc3074
Naoki MORITA <nqo.morita@gmail.com>
2023-08-14 17:32:53 +0900

    Fix gedatsu_dlb_get_conn_graph_comm_table

--- src/dlb/dlb_comm_conn.f90
+++ src/dlb/dlb_comm_conn.f90
@@ -3,8 +3,8 @@ module mod_gedatsu_dlb_comm_conn
   use mod_monolis_utils
   use mod_gedatsu_graph
   use mod_gedatsu_dlb
-  use mod_gedatsu_graph_repart
   use mod_gedatsu_dlb_comm_nodal
+  use mod_gedatsu_graph_repart
   implicit none
 
 contains
@@ -25,8 +25,8 @@ contains
     integer(kint) :: i, comm_size, my_rank, no_use
     integer(kint), allocatable :: is_send_nodal_vertex(:)
     integer(kint), allocatable :: domain_id_org(:)
-    integer(kint), allocatable :: send_n_conn_list(:)
-    integer(kint), allocatable :: recv_n_conn_list(:)
+    integer(kint), allocatable :: send_n_node_list(:)
+    integer(kint), allocatable :: recv_n_node_list(:)
     integer(kint), allocatable :: send_n_edge_list(:)
     integer(kint), allocatable :: recv_n_edge_list(:)
 
@@ -40,40 +40,43 @@ contains
 
     do i = 1, comm_size
       if(my_rank == i - 1) cycle
-
+      !# 各領域番号に送る要素とエッジ数を格納
       call monolis_alloc_I_1d(is_send_nodal_vertex, nodal_graph%n_vertex)
       call gedatsu_dlb_get_n_move_vertex(nodal_graph, no_use, &
         & is_send_nodal_vertex, domain_id_org, i - 1)
 
-      call monolis_alloc_I_1d(update_db(i)%is_send_node, nodal_graph%n_vertex)
+      call monolis_alloc_I_1d(update_db(i)%is_send_node, conn_graph%n_vertex)
+      call monolis_alloc_I_1d(update_db(i)%is_send_edge, conn_graph%index(conn_graph%n_vertex + 1))
       call gedatsu_dlb_get_n_move_connectivity(conn_graph, is_send_nodal_vertex, &
+        & update_db(i)%n_send_node, update_db(i)%is_send_node, &
         & update_db(i)%n_send_edge, update_db(i)%is_send_edge)
     enddo
 
     !# 送信計算点（要素）の全体情報取得
-!    call monolis_alloc_I_1d(send_n_conn_list, comm_size)
-!    call monolis_alloc_I_1d(recv_n_conn_list, comm_size)
+    call monolis_alloc_I_1d(send_n_node_list, comm_size)
+    call monolis_alloc_I_1d(recv_n_node_list, comm_size)
 
-!    do i = 1, comm_size
-!      send_n_conn_list(i) = update_db(i)%n_send_node
-!    enddo
+    do i = 1, comm_size
+      send_n_node_list(i) = update_db(i)%n_send_node
+    enddo
 
-!    recv_n_conn_list = send_n_conn_list
-!    call monolis_alltoall_I1(comm_size, recv_n_conn_list, COM%comm)
+    recv_n_node_list = send_n_node_list
+    call monolis_alltoall_I1(comm_size, recv_n_node_list, COM%comm)
 
     !# エッジ（コネクティビティ）の全体情報取得
-!    call monolis_alloc_I_1d(send_n_edge_list, comm_size)
-!    call monolis_alloc_I_1d(recv_n_edge_list, comm_size)
+    call monolis_alloc_I_1d(send_n_edge_list, comm_size)
+    call monolis_alloc_I_1d(recv_n_edge_list, comm_size)
 
-!    do i = 1, comm_size
-!      send_n_edge_list(i) = update_db(i)%n_send_edge
-!    enddo
+    do i = 1, comm_size
+      send_n_edge_list(i) = update_db(i)%n_send_edge
+    enddo
 
-!    recv_n_edge_list = send_n_edge_list
-!    call monolis_alltoall_I1(comm_size, recv_n_edge_list, COM%comm)
+    recv_n_edge_list = send_n_edge_list
+    call monolis_alltoall_I1(comm_size, recv_n_edge_list, COM%comm)
 
-!    call gedatsu_dlb_generate_conn_graph_comm_table(dlb, conn_graph, update_db, &
-!      & send_n_conn_list, recv_n_conn_list, COM)
+    call gedatsu_dlb_generate_nodal_graph_comm_table(dlb, conn_graph, update_db, &
+      & send_n_node_list, recv_n_node_list, &
+      & send_n_edge_list, recv_n_edge_list, COM)
   end subroutine gedatsu_dlb_get_conn_graph_comm_table
 
   !> @ingroup group_dlb
@@ -284,122 +287,26 @@ contains
   end subroutine gedatsu_dlb_get_temporary_conn_graph
 
   !> @ingroup group_dlb
-  !> 動的負荷分散のための付随グラフ構造アップデート
-  subroutine gedatsu_dlb_generate_conn_graph_comm_table(dlb, conn_graph, update_db, &
-    & send_n_conn_list, recv_n_conn_list, COM)
-    implicit none
-    !> [in] dlb 構造体
-    type(gedatsu_dlb), intent(inout) :: dlb
-    !> [in] graph 構造体
-    type(gedatsu_graph), intent(in) :: conn_graph
-    !> [in] update_db 構造体
-    type(gedatsu_update_db) :: update_db(:)
-    !> [in]
-    integer(kint) :: send_n_conn_list(:)
-    !> [in]
-    integer(kint) :: recv_n_conn_list(:)
-    !> [in] COM 構造体
-    type(monolis_COM), intent(in) :: COM
-    integer(kint) :: i, in, j, n_item
-    integer(kint) :: my_rank, comm_size
-
-    call monolis_com_finalize(dlb%COM_node)
-    call monolis_com_finalize(dlb%COM_edge)
-
-    my_rank = monolis_mpi_get_local_my_rank(COM%comm)
-    comm_size = monolis_mpi_get_local_comm_size(COM%comm)
-
-    dlb%COM_node%comm = COM%comm
-    dlb%COM_node%my_rank = COM%my_rank
-    dlb%COM_node%comm_size = COM%comm_size
-
-    !# node セクション
-    !# send table の作成
-    n_item = 0
-    do i = 1, comm_size
-      if(send_n_conn_list(i) /= 0)then
-        dlb%COM_node%send_n_neib = dlb%COM_node%send_n_neib + 1
-        n_item = n_item + send_n_conn_list(i)
-      endif
-    enddo
-
-    if(dlb%COM_node%send_n_neib == 0)then
-      call monolis_palloc_I_1d(dlb%COM_node%send_neib_pe, 1)
-      call monolis_palloc_I_1d(dlb%COM_node%send_index, 2)
-      call monolis_palloc_I_1d(dlb%COM_node%send_item, 1)
-    else
-      call monolis_palloc_I_1d(dlb%COM_node%send_neib_pe, dlb%COM_node%send_n_neib)
-      call monolis_palloc_I_1d(dlb%COM_node%send_index, dlb%COM_node%send_n_neib + 1)
-      call monolis_palloc_I_1d(dlb%COM_node%send_item, n_item)
-    endif
-
-    in = 0
-    do i = 1, comm_size
-      if(send_n_conn_list(i) /= 0)then
-        in = in + 1
-        dlb%COM_node%send_neib_pe(in) = i - 1
-        dlb%COM_node%send_index(in + 1) = dlb%COM_node%send_index(in) + send_n_conn_list(i)
-      endif
-    enddo
-
-    in = 0
-    do i = 1, comm_size
-      do j = 1, conn_graph%n_vertex
-        if(update_db(i)%n_send_node == 0) cycle
-        if(update_db(i)%is_send_node(j) == 1)then
-          in = in + 1
-          dlb%COM_node%send_item(in) = j
-        endif
-      enddo
-    enddo
-
-    !# recv table の作成
-    n_item = 0
-    do i = 1, comm_size
-      if(recv_n_conn_list(i) /= 0)then
-        dlb%COM_node%recv_n_neib = dlb%COM_node%recv_n_neib + 1
-        n_item = n_item + recv_n_conn_list(i)
-      endif
-    enddo
-
-    if(dlb%COM_node%recv_n_neib == 0)then
-      call monolis_palloc_I_1d(dlb%COM_node%recv_neib_pe, 1)
-      call monolis_palloc_I_1d(dlb%COM_node%recv_index, 2)
-      call monolis_palloc_I_1d(dlb%COM_node%recv_item, 1)
-    else
-      call monolis_palloc_I_1d(dlb%COM_node%recv_neib_pe, dlb%COM_node%recv_n_neib)
-      call monolis_palloc_I_1d(dlb%COM_node%recv_index, dlb%COM_node%recv_n_neib + 1)
-      call monolis_palloc_I_1d(dlb%COM_node%recv_item, n_item)
-    endif
-
-    in = 0
-    do i = 1, comm_size
-      if(recv_n_conn_list(i) /= 0)then
-        in = in + 1
-        dlb%COM_node%recv_neib_pe(in) = i - 1
-        dlb%COM_node%recv_index(in + 1) = dlb%COM_node%recv_index(in) + recv_n_conn_list(i)
-      endif
-    enddo
-
-    call monolis_get_sequence_array_I(dlb%COM_node%recv_item, n_item, 1, 1)
-  end subroutine gedatsu_dlb_generate_conn_graph_comm_table
-
-  !> @ingroup group_dlb
   !> オーバーラップ計算点を含む通信するエッジ数の取得
-  subroutine gedatsu_dlb_get_n_move_connectivity(graph, is_send_node, n_move_edge, is_send_edge)
+  subroutine gedatsu_dlb_get_n_move_connectivity(graph, is_send_nodal_vertex, &
+    & n_send_node, is_send_node, n_send_edge, is_send_edge)
     implicit none
     !> [in] graph 構造体
     type(gedatsu_graph), intent(in) :: graph
     !> [in] オーバーラップ計算点を含む通信する節点のフラグ
+    integer(kint) :: is_send_nodal_vertex(:)
+    !> [in] オーバーラップ計算点を含む通信するエッジ数
+    integer(kint) :: n_send_node
+    !> [in] オーバーラップ計算点を含む通信するエッジ数
     integer(kint) :: is_send_node(:)
     !> [in] オーバーラップ計算点を含む通信するエッジ数
-    integer(kint) :: n_move_edge
+    integer(kint) :: n_send_edge
     !> [in] オーバーラップ計算点を含む通信するエッジのフラグ
     integer(kint) :: is_send_edge(:)
     integer(kint) :: i, j, jS, jE, jn
     logical :: is_move_tmp
 
-    is_send_edge = 0
+    is_send_node = 0
 
     do i = 1, graph%n_vertex
       jS = graph%index(i) + 1
@@ -407,16 +314,28 @@ contains
       is_move_tmp = .true.
       do j = jS, jE
         jn = graph%item(j)
-        if(is_send_node(jn) == 0)then
+        if(is_send_nodal_vertex(jn) == 0)then
           is_move_tmp = .false.
         endif
       enddo
-      if(is_move_tmp) is_send_edge(i) = 1
+      if(is_move_tmp) is_send_node(i) = 1
     enddo
 
-    n_move_edge = 0
+    n_send_node = 0
     do i = 1, graph%n_vertex
-      if(is_send_edge(i) == 1) n_move_edge = n_move_edge + 1
+      if(is_send_node(i) == 1) n_send_node = n_send_node + 1
+    enddo
+
+    n_send_edge = 0
+    is_send_edge = 0
+    do i = 1, graph%n_vertex
+      if(is_send_node(i) == 0) cycle
+      jS = graph%index(i) + 1
+      jE = graph%index(i + 1)
+      do j = jS, jE
+        n_send_edge = n_send_edge + 1
+        is_send_edge(j) = 1
+      enddo
     enddo
   end subroutine gedatsu_dlb_get_n_move_connectivity
 
