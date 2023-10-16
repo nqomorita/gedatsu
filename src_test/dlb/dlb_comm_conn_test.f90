a92eb14df5acc1f01854e0358563837b6ef23925
Naoki MORITA <nqo.morita@gmail.com>
2023-10-16 15:47:05 +0900

    Fix module name

--- src_test/dlb/dlb_comm_conn_test.f90
+++ src_test/dlb/dlb_comm_conn_test.f90
@@ -1,7 +1,7 @@
 !> 動的負荷分散テストモジュール
-module mod_gedatsu_dlb_comm_test
+module mod_gedatsu_dlb_comm_conn_test
   implicit none
 
 contains
 
-end module mod_gedatsu_dlb_comm_test
+end module mod_gedatsu_dlb_comm_conn_test
